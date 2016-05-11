dabb3ee170aeea4a39b1c7b06a4cdd8b3d6950c7
Reuben Puketapu <reuben.puketapu@gmail.com>
2016-05-12 11:37:48 +1200

    Added image and now prints to console user and pass

--- login.js
+++ login.js
@@ -3,15 +3,15 @@ $(document).ready(function(e) {
 	//var loginPass = $('#p1').val();
 	//var loginPass2 = document.getElementById("p1");
 
-	var loginName = document.getElementById("t1");
-		var loginPass = document.getElementById("p1");
+	
+	
+
+	$(('#Submit')).on('click', function(){
+		var loginName = $('#t1').val();
+		var loginPass = $('#p1').val();
 
 		console.log(loginName);
 		console.log(loginPass);
-	
-
-	$('#sub').on('click', function(){
-		
 
 	});
 	
