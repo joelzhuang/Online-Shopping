c2f9c7386d9e7493f92e18bd7b781d3ecd73cc55
Reuben Puketapu <reuben.puketapu@gmail.com>
2016-05-12 11:14:22 +1200

    Added javascript files and login html/js

---
+++ login.js
@@ -0,0 +1,19 @@
+$(document).ready(function(e) {
+
+	//var loginPass = $('#p1').val();
+	//var loginPass2 = document.getElementById("p1");
+
+	var loginName = document.getElementById("t1");
+		var loginPass = document.getElementById("p1");
+
+		console.log(loginName);
+		console.log(loginPass);
+	
+
+	$('#sub').on('click', function(){
+		
+
+	});
+	
+
+});
