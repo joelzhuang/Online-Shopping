9bb9f90bdc1eda4171f9b0e16e5f28117746b0a0
huangjoel <huangjoel@the-taj.ecs.vuw.ac.nz>
2016-05-18 14:01:39 +1200

    added day,month,year to registeration page

--- register.js
+++ register.js
@@ -11,28 +11,23 @@ $(document).ready(function(e) {
 		var namReg=/^[a-zA-Z]{4,15}$/;
    		var emailReg=/^[a-zA-Z0-9-_\.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/;
 	    var passReg=/^[a-zA-Z0-9-_]{6,16}$/;
-		var phnReg=/^[0-9]{9,14}$/;
+		var phnReg=/^[0-9]{6,14}$/;
 		var addReg=/^[a-zA-Z0-9 ]{10,150}$/;
 		var cityReg=/^[a-zA-Z]{5,30}$/;
 
 		var title = $('#sel1').val();
 		var fname = $('#t1').val();
 		var lname = $('#t2').val();
-		var gender = $('#r1').val();
 		var email = $('#t3').val();
 		var password = $('#p1').val();
 		var phone = $('#t5').val();
 		var address = $('#t6').val();
 		var country = $('#t8').val();
-		var birth = $('#t9').val();
 		if (!fname.match(namReg)) { 
-			alert("Please enter your first name");
+			alert("Please enter your first name" + gender);
 			return false; 
 		} else if (!lname.match(namReg)) { 
 			alert("Please enter your last name");
-			return false; 
-		} else if (gender === '') { 
-			alert("Please select gender type");
 			return false; 
 		} else if (!email.match(emailReg)) { 
 			alert("Please enter a valid email address");
@@ -48,9 +43,6 @@ $(document).ready(function(e) {
 			return false; 
 		} else if (!country.match(addReg)) { 
 			alert("Please enter your country");
-			return false; 
-		} else if (!birth === '') { 
-			alert("Please enter your date of birth");
 			return false; 
 		} 
 
